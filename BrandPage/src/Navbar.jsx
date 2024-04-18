90119da02da310227828041229191091cfbdbb9a
Sushil <kidsushilink@gmail.com>
2024-04-17 23:33:14 -0700

    update 1
    -- added image
    -- ui design for navbar

---
+++ BrandPage/src/Navbar.jsx
@@ -0,0 +1,27 @@
+
+
+const Navbar = () => {
+
+    return(
+        <>
+        <nav>
+            <div className="logo">
+                <img src="/image/brand-logo.png" alt="logo" />
+            </div>
+
+            <ul>
+                <li href="">Menu</li>
+                <li href="">Location</li>
+                <li href="">About</li>
+                <li href="">Contact</li>
+            </ul>
+
+            <button>Login</button>
+        </nav>
+
+        </>
+    );
+
+}
+
+export default Navbar;
