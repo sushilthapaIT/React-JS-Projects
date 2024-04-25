fe63d4ea3054c08c17698ad44a73e62138ebfd5b
Sushil <kidsushilink@gmail.com>
2024-04-25 01:14:20 -0700

    Contact Page Initial Commit
    -- UI of contact page
    -- react

---
+++ ContactPage/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+ import styles from './Navbar.module.css'
+
+const Navbar = () => {
+    return(<>
+    <nav className="container">
+
+        <div className="logo">
+            <img src="images/logo.png"  alt="logo"/>
+        </div>
+
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+
+    </nav>
+    </>)
+}
+
+export default Navbar
