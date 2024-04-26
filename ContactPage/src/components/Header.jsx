eec9951e61f521bc685db912c262c5522acc7b4d
Sushil <kidsushilink@gmail.com>
2024-04-26 00:43:49 -0700

    Created ContactForm
    -- making ui

--- ContactPage/src/components/Header.jsx
+++ ContactPage/src/components/Header.jsx
@@ -1,9 +1,14 @@
-
+import styles from './Navigation/Header.module.css'
 
 const Header = () => {
     return(
         <>
-            <h1>Hi i am a header of a contact page</h1>
+            <div>
+                <h1>CONTACT US</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat molestiae odio minus quo fugit voluptatum modi amet, officiis, quibusdam magni rerum excepturi omnis odit culpa esse corporis, inventore veritatis dicta!
+                </p>
+            </div>
         </>
     );
 }
