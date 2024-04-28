c82015f2d8d40b974dce3863784ec07d40c04f9e
Sushil <kidsushilink@gmail.com>
2024-04-27 23:41:45 -0700

    Created new Component named as GamePlay

--- ContactForm/src/App.jsx
+++ ContactForm/src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css'
 import ContactForm from './ContactForm'
+import React, {useState} from 'react'
 
 function App() {
+
+  const [isGameStarted, setIsGameStarted] = useState(false);
+
+  const toggleGamePlay = () => {
+    setIsGameStarted((previousValue) => !previousValue)
+  }
 
   return (
     <>
