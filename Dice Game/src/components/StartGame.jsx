ec5dd40cbbcc720b11967e3f8c2853cfa2013fbe
Sushil <kidsushilink@gmail.com>
2024-04-27 23:35:10 -0700

    button updated

--- Dice Game/src/components/StartGame.jsx
+++ Dice Game/src/components/StartGame.jsx
@@ -7,7 +7,7 @@ const StartGame = () => {
         <img src='dices.png' alt='dice image'/>
       </div>
 
-      <div>
+      <div className='content'>
         <h1>DICE GAME</h1>
         <Button>Play Now</Button>
 
@@ -24,6 +24,13 @@ const Container = styled.div`
   display: flex;
   margin: 0 auto;
   height: 100vh;
+  align-items: center;
+
+  .content{
+    h1{
+      font-size: 96px;
+    }
+  }
 `;
 
 const Button = styled.button`
@@ -33,4 +40,16 @@ const Button = styled.button`
   border-radius: 5px;
   min-width: 220px;
   border: none;
+  font-size: 16px;
+  cursor: pointer;
+  border: 1px solid transparent;
+  transition: 0.4s background ease-in;
+
+
+  &:hover{
+    background-color: white;
+    border: 1px solid black;
+    color: black; 
+    transition: 0.3s background ease-in;
+  }
 `
