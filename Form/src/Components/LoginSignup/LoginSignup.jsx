ebac624f04b06e7c7e6f181724454347683abda7
Sushil <kidsushilink@gmail.com>
2024-05-08 23:43:24 -0700

    New project
    to create a form in React

---
+++ Form/src/Components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './LoginSignup.css'
+
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+
+const LoginSignup = () => {
+  return (
+    <div className='container'>
+        <div className="header">
+            <div className="text">Sign Up</div>
+            <div className="underline"></div>
+        </div>
+
+        <div className="inputs">
+            <div className="input">
+                <img src="" alt="" />
+                <input type="text" />
+            </div>
+
+            <div className="input">
+                <img src="" alt="" />
+                <input type="text" />
+            </div>
+            
+            <div className="input">
+                <img src="" alt="" />
+                <input type="text" />
+            </div>
+        </div>
+        
+      
+    </div>
+  )
+}
+
+export default LoginSignup
