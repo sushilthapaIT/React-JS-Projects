8cdbd9785529137e7ba10dd3919ae21d0530c963
Sushil <kidsushilink@gmail.com>
2024-05-10 23:44:33 -0700

    added image source

--- Form/src/Components/LoginSignup/LoginSignup.jsx
+++ Form/src/Components/LoginSignup/LoginSignup.jsx
@@ -15,17 +15,17 @@ const LoginSignup = () => {
 
         <div className="inputs">
             <div className="input">
-                <img src="" alt="" />
+                <img src={user_icon} alt="" />
                 <input type="text" />
             </div>
 
             <div className="input">
-                <img src="" alt="" />
+                <img src={email_icon} alt="" />
                 <input type="email" />
             </div>
 
             <div className="input">
-                <img src="" alt="" />
+                <img src={password_icon} alt="" />
                 <input type="password" />
             </div>
         </div>
