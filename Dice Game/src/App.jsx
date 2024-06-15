6759b5fa2d040739acb34113133aea0880e37c55
Sushil <kidsushilink@gmail.com>
2024-06-15 14:52:26 -0700

    removed all the old code
    -- removing all the previous old code and writing the same code again

--- Dice Game/src/App.jsx
+++ Dice Game/src/App.jsx
@@ -1,19 +1,10 @@
-import StartGame from './components/StartGame.jsx'; 
-import GamePlay from './components/GamePlay.jsx'; 
-import React, { useState } from 'react';
 
 function App() {
-  const [isGameStarted, setIsGameStarted] = useState(true);
 
-  const toggleGamePlay = () => {
-    setIsGameStarted((previous) => !previous);
-  };
-
-  return (
-    <>
-      {isGameStarted ? <GamePlay /> : <StartGame toggle={toggleGamePlay} />}
-    </>
-  );
+  <>
+  
+  </>
+  
 }
 
 export default App;
