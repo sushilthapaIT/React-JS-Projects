e14531ae4c648710167bad5ef2c54fa52fd4814f
Sushil <kidsushilink@gmail.com>
2024-06-15 15:09:11 -0700

    Project Setup
    -- project setup to build a game called dice roll which let user to pick the number and if it matches with the computer then they will get the point. Also, it shows deduct the point from the user and shows error message if they do not select the number and try to play.

---
+++ Dice-ROll Game/src/App.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+function App() {
+
+  return (
+    <>
+      <Button>I am button</Button>
+    </>
+  )
+}
+
+export default App
+
+
+const Button = styled.button`
+  background-color: #e73434;
+  border: none;
+  padding: 20px;
+  
+`
