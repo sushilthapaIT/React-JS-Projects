acf44ff499aa0e1dfe32c998efe154017f692756
Sushil <kidsushilink@gmail.com>
2024-06-16 06:50:12 -0700

    update in numberselector.jsx

--- Dice-ROll Game/src/components/NumberSelector.jsx
+++ Dice-ROll Game/src/components/NumberSelector.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
 const NumberSelector = () => {
-  return (
+    const arrNumber = [1, 2, 3, 4, 5, 6];
+
+    const [selectedNumber, setSelectedNumber] = useState();
+    return (
     <div>
-      
+        {arrNumber.map((value, index) => (
+        <Box key={index}>{value}</Box>
+        ))}
     </div>
-  )
+);
 }
 
-export default NumberSelector
+export default NumberSelector;
+
+const Box = styled.div`
+    height: 72px;
+    width: 72px;
+    border: 1px solid black;
+    display: grid;
+    place-items: center;
+    font-size: 24px;
+    font-weight: 700;
+`;
