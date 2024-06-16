3cb8c69e221ccbc3bfd482c1385124b30a305c42
Sushil <kidsushilink@gmail.com>
2024-06-16 07:31:36 -0700

    adding props in number selector, adding props, using usestate in real time example

--- Dice-ROll Game/src/components/NumberSelector.jsx
+++ Dice-ROll Game/src/components/NumberSelector.jsx
@@ -5,10 +5,11 @@ const NumberSelector = () => {
     const arrNumber = [1, 2, 3, 4, 5, 6];
 
     const [selectedNumber, setSelectedNumber] = useState();
+
     return (
     <div>
         {arrNumber.map((value, index) => (
-        <Box key={index}>{value}</Box>
+        <Box isSelected={value === selectedNumber} key={index} onClick={() => setSelectedNumber(value)}>{value}</Box>
         ))}
     </div>
 );
@@ -24,4 +25,6 @@ const Box = styled.div`
     place-items: center;
     font-size: 24px;
     font-weight: 700;
+    background-color: ${(props) => (props.isSelected ? "black" : "white")};
+    color: ${(props) => (!props.isSelected ? "black" : "white")};
 `;
