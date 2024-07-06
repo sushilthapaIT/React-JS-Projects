3e0a6b7994c06b87a3dbdf47016ce2054fc6eccc
Sushil <kidsushilink@gmail.com>
2024-07-06 10:57:52 -0700

    added colors in tailwind.config
    -- so that we can reuse the same color again and again

--- CRUD firebase/tailwind.config.js
+++ CRUD firebase/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: "#5A5959",
+        yellow: "#FFEAAE",
+        "dark-yellow": "#FCCA3F",
+        orange: "#F6820C",
+      },
+    },
   },
   plugins: [],
 }
