019353d4665842aea1f350c0eb4f71138e017686
Sushil <kidsushilink@gmail.com>
2024-07-06 11:59:04 -0700

    added content in nav.jsx
    -- content added all
    -- used react icons, tailwind css to make the content

--- CRUD firebase/src/App.jsx
+++ CRUD firebase/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import {FiSearch} from "react-icons/fi";
+import {AiFillPlusCircle} from "react-icons/ai"
 import Nav from './components/Nav';
 
 const App = () => {
   return (
     <>
-    <Nav />
+    <div className='max-w-[370px] mx-auto px-4'>
+      <Nav />
+    <div className='flex gap-2'>
+      <div className='relative flex-grow flex items-center ml-1'>
+          <FiSearch className='absolute text-3xl text-white'/>
+          <input type='text' className='flex-grow border bg-transparent border-white rounded-md h-10 text-white pl-9'/>
+      </div>
+      <AiFillPlusCircle className='text-5xl cursor-pointer text-white'/>
+    </div>
+    </div>
     </>
   )
 }
--- CRUD firebase/src/components/Nav.jsx
+++ CRUD firebase/src/components/Nav.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const Nav = () => {
   return (
-    <div>
-      
+    <div className='h-[60px] bg-white gap-2 my-4 rounded-lg flex items-center justify-center text-xl font-medium'>
+      <img src='/firebase.svg' alt='image of firebase' />
+      <h1>Firebase Contact App</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
