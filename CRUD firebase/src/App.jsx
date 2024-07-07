c3328a78b2984c3c6314ccc01e87d1823701dc26
Sushil <kidsushilink@gmail.com>
2024-07-07 15:01:23 -0700

    Work in progress to fetch the data from the firebase
    -- working in fronend and backend to get the data from firebase

--- CRUD firebase/src/App.jsx
+++ CRUD firebase/src/App.jsx
@@ -1,44 +1,57 @@
 import React, { useEffect, useState } from 'react'
-import {FiSearch} from "react-icons/fi";
-import {AiFillPlusCircle} from "react-icons/ai"
+import { FiSearch } from "react-icons/fi";
+import { AiFillPlusCircle } from "react-icons/ai";
+import {HiOutlineUserCircle} from "react-icons/hi"
 import Nav from './components/Nav';
-import { collection, getDoc } from 'firebase/firestore';
-import {db} from "./config/firebase";
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from "./config/firebase";
 
 const App = () => {
 
   const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
-
     const getContacts = async () => {
-      try{
+      try {
         const contactsRef = collection(db, "contacts");
-        const contactsSnapShot = await getDoc(contactsRef);
-        const contactLists = contacts.Snapshot.docs.map((doc) => doc.data());
-        console.log(contactsSnapShot)
-      }
-      catch (error){
+        const contactsSnapShot = await getDocs(contactsRef);
+        const contactLists = contactsSnapShot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        setContacts(contactLists);
+      } catch (error) {
         console.log("Error!!!", error);
       }
-    }
+    };
 
     getContacts();
-  }, [])
+  }, []);
+
   return (
     <>
-    <div className='max-w-[370px] mx-auto px-4'>
-      <Nav />
-    <div className='flex gap-2'>
-      <div className='relative flex-grow flex items-center ml-1'>
-          <FiSearch className='absolute text-3xl text-white'/>
-          <input type='text' className='flex-grow border bg-transparent border-white rounded-md h-10 text-white pl-9'/>
+      <div className='max-w-[370px] mx-auto px-4'>
+        <Nav />
+        <div className='flex gap-2'>
+          <div className='relative flex-grow flex items-center ml-1'>
+            <FiSearch className='absolute text-3xl text-white' />
+            <input type='text' className='flex-grow border bg-transparent border-white rounded-md h-10 text-white pl-9' />
+          </div>
+          <AiFillPlusCircle className='text-5xl cursor-pointer text-white' />
+        </div>
+        <div>
+          {contacts.map((contact) => (
+            <div key={contact.id}>
+              <HiOutlineUserCircle />
+              {/* You can display other contact details here */}
+            </div>
+          ))}
+        </div>
       </div>
-      <AiFillPlusCircle className='text-5xl cursor-pointer text-white'/>
-    </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
