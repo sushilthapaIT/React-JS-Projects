4ddbac7691cbeb0c23d85a91e3c07ae36e4b76b9
Sushil <kidsushilink@gmail.com>
2024-07-15 12:02:18 -0700

    update

--- CRUD firebase/src/components/Added.jsx
+++ CRUD firebase/src/components/Added.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+// import React from 'react'
 
-const Added = () => {
-  return (
-    <div>
+// const Added = () => {
+//   return (
+//     <div>
       
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
-export default Added
+// export default Added
