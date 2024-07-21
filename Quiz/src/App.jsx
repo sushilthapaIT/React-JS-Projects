0f79ddbc40ea94c0351fd8d2a54c3e8e41c223d2
Sushil <kidsushilink@gmail.com>
2024-07-21 13:58:29 -0700

    content rendering of application
    -- checking the content

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -2,7 +2,22 @@ function App() {
 
   return (
     <>
+      <div className="App">
+        <h1>Quiz App</h1>
 
+        <h2>Current Score: 2</h2>
+
+        <div className="question-card">
+          <h2>Question 1 out of 5</h2>
+          <h3>What is the capital of USA?</h3>
+          <ul>
+            <li>Boston</li>
+            <li>Ontario</li>
+            <li>Washington DC</li>
+            <li>Denver</li>
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
