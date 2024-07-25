927564b5125818ff4be10b3ef81a46f3528a90f4
Sushil <kidsushilink@gmail.com>
2024-07-24 21:35:05 -0700

    removed all the unnecessary comment

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -1,27 +1,27 @@
-// import './App.css';
+import './App.css';
 
-// function App() {
+function App() {
 
-//   return (
-//     <>
-//       <div className="App">
-//         <h1>Quiz App</h1>
+  return (
+    <>
+      <div className="App">
+        <h1>Quiz App</h1>
 
-//         <h2>Current Score: 2</h2>
+        <h2>Current Score: 2</h2>
 
-//         <div className="question-card">
-//           <h2>Question 1 out of 5</h2>
-//           <h3>What is the capital of USA?</h3>
-//           <ul>
-//             <li>Boston</li>
-//             <li>Ontario</li>
-//             <li>Washington DC</li>
-//             <li>Denver</li>
-//           </ul>
-//         </div>
-//       </div>
-//     </>
-//   )
-// }
+        <div className="question-card">
+          <h2>Question 1 out of 5</h2>
+          <h3>What is the capital of USA?</h3>
+          <ul>
+            <li>Boston</li>
+            <li>Ontario</li>
+            <li>Washington DC</li>
+            <li>Denver</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+}
 
-// export default App
+export default App
