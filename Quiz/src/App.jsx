73bba9555557b9e067e80fdd2ba0f4305b5dee03
Sushil <kidsushilink@gmail.com>
2024-07-27 00:24:13 -0700

    added conditional rendering
    -- showing the final result if all question were answered if not showing all the questions

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [showFinalResult, setFinalResult] = useState(false);
 
   return (
     <>
       <div className="App">
         <h1>Quiz App</h1>
-
         <h2>Current Score: 2</h2>
-
+        {showFinalResult ?     // conditional rendering     
+        (
+        <div className="final-results">
+          <h1>Final Results</h1>
+          <h2>1 out of 5 correct - (20%)</h2>
+          <button>Restart Game</button>
+        </div>
+        )
+        :
+        (         
         <div className="question-card">
           <h2>Question 1 out of 5</h2>
           <h3 className="question-text">What is the capital of USA?</h3>
@@ -18,16 +28,11 @@ function App() {
             <li>Washington DC</li>
             <li>Denver</li>
           </ul>
-        </div>
-
-        <div className="final-results">
-          <h1>Final Results</h1>
-          <h2>1 out of 5 correct - (20%)</h2>
-          <button>Restart Game</button>
-        </div>
+        </div> 
+        )}
       </div>
     </>
   )
 }
 
-export default App
+export default App;
