faeae65e7d04b7b9bae20c92800fa5d0031a5ef3
Sushil <kidsushilink@gmail.com>
2024-07-27 00:35:31 -0700

    -- addded arrays of questions

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -3,7 +3,57 @@ import React, {useState} from 'react';
 
 function App() {
   const [showFinalResult, setFinalResult] = useState(false);
+  const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
 
+  const questions = [
+      {
+        text: "What is the capital of America?",
+        options: [
+          { id: 0, text: "New York City", isCorrect: false },
+          { id: 1, text: "Boston", isCorrect: false },
+          { id: 2, text: "Santa Fe", isCorrect: false },
+          { id: 3, text: "Washington DC", isCorrect: true },
+        ],
+      },
+      {
+        text: "What year was the Constitution of America written?",
+        options: [
+          { id: 0, text: "1787", isCorrect: true },
+          { id: 1, text: "1776", isCorrect: false },
+          { id: 2, text: "1774", isCorrect: false },
+          { id: 3, text: "1826", isCorrect: false },
+        ],
+      },
+      {
+        text: "Who was the second president of the US?",
+        options: [
+          { id: 0, text: "John Adams", isCorrect: true },
+          { id: 1, text: "Paul Revere", isCorrect: false },
+          { id: 2, text: "Thomas Jefferson", isCorrect: false },
+          { id: 3, text: "Benjamin Franklin", isCorrect: false },
+        ],
+      },
+      {
+        text: "What is the largest state in the US?",
+        options: [
+          { id: 0, text: "California", isCorrect: false },
+          { id: 1, text: "Alaska", isCorrect: true },
+          { id: 2, text: "Texas", isCorrect: false },
+          { id: 3, text: "Montana", isCorrect: false },
+        ],
+      },
+      {
+        text: "Which of the following countries DO NOT border the US?",
+        options: [
+          { id: 0, text: "Canada", isCorrect: false },
+          { id: 1, text: "Russia", isCorrect: true },
+          { id: 2, text: "Cuba", isCorrect: true },
+          { id: 3, text: "Mexico", isCorrect: false },
+        ],
+      },
+    ];  
+  
   return (
     <>
       <div className="App">
@@ -20,7 +70,7 @@ function App() {
         :
         (         
         <div className="question-card">
-          <h2>Question 1 out of 5</h2>
+          <h2>Question {currentQuestion + 1} out of {questions.length}</h2>
           <h3 className="question-text">What is the capital of USA?</h3>
           <ul>
             <li>Boston</li>
