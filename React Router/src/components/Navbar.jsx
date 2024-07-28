10b5d4f23f9b44fb13155a2e1151b27502ba1f18
Sushil <kidsushilink@gmail.com>
2024-07-27 19:35:36 -0700

    created navbar component

---
+++ React Router/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {createBrowserRouter} from 'react-router-dom';
+
+const Navbar = () => {
+
+    return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
