9fe69b67394cbe3f2b843706c186316b56794ed8
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-10-09 09:38:33 -0400

    commit 1

---
+++ String Conversion/src/App.jsx
@@ -0,0 +1,12 @@
+import StringConvert from "./StringConvert"
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{color: 'red'}}>String Convertor</h1>
+      <StringConvert />
+    </div>
+  )
+}
+
+export default App
---
+++ String Conversion/src/StringConvert.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StringConvert = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default StringConvert
